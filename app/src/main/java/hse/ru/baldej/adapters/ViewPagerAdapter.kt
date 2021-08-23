086ab551e899c6a782d62d3c21f541d3d57c3d3b
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-08-23 23:46:53 +0300

    add history

--- app/src/main/java/hse/ru/baldej/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/hse/ru/baldej/adapters/ViewPagerAdapter.kt
@@ -3,6 +3,8 @@ package hse.ru.baldej.adapters
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import hse.ru.baldej.ui.fragments.CompanionsFragment
+import hse.ru.baldej.ui.fragments.MyTripsFragment
 
 class ViewPagerAdapter(fragmentManager: Fragment) : FragmentStateAdapter(fragmentManager) {
 
@@ -12,7 +14,7 @@ class ViewPagerAdapter(fragmentManager: Fragment) : FragmentStateAdapter(fragmen
         Log.i("adapter", "ya tut bil")
         return when (position) {
             0 -> MyTripsFragment()
-            1 -> TravelersFragment()
+            1 -> CompanionsFragment()
             else -> MyTripsFragment()
         }
 
--- app/src/main/java/hse/ru/baldej/ui/fragments/CompanionsFragment.kt
+++ app/src/main/java/hse/ru/baldej/ui/fragments/CompanionsFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import hse.ru.baldej.R
+import hse.ru.baldej.databinding.FragmentCompanionsBinding
 import hse.ru.baldej.databinding.FragmentLoginBinding
-import hse.ru.baldej.databinding.FragmentMyTripsBinding
 
 
-
-class MyTripsFragment : Fragment() {
-    private lateinit var binding: FragmentMyTripsBinding
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CompanionsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CompanionsFragment : Fragment() {
+    private lateinit var binding: FragmentCompanionsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +27,7 @@ class MyTripsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentMyTripsBinding.inflate(inflater,container,false)
+        binding = FragmentCompanionsBinding.inflate(inflater,container,false)
         return binding.root
     }
 
@@ -35,10 +39,15 @@ class MyTripsFragment : Fragment() {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CompanionsFragment.
          */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MyTripsFragment().apply {
+            CompanionsFragment().apply {
                 arguments = Bundle().apply {
 //                    putString(ARG_PARAM1, param1)
 //                    putString(ARG_PARAM2, param2)
